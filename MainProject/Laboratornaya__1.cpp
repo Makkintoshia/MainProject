e47c39946c90d930a7b7430c0c23fdaf5df10dba
Валентин Сотников <valentin.sotniko@mail.ru>
2021-07-01 16:42:12 +0300

    Добавьте файлы проекта.

---
+++ MainProject/Laboratornaya__1.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+const double pi = 3.14159265;
+const double e = 2.71828;
+int Laboratornaya__1()
+{
+	double a;
+	double b;
+	double x;
+	double z;
+
+
+	//Ввод переменных с консоли
+
+	cout << "Write a";
+	cin >> a;
+	cout << "Write b";
+	cin >> b;
+	cout << "Write x";
+	cin >> x;
+
+	//Вычесление z
+
+	if (x <= a)
+	{
+		z = (1 + x * x) / (2 - 11 * sin(x)) + pow(e, sinh(x));
+	}
+
+	else if (x > a && x < b)
+	{
+		z = sqrt(1.57 - pow(x, 3) * pow(sin(x), 2)) + 4.1 * pow(e, (2 * x));
+	}
+
+	else if (x >= b)
+	{
+		z = pow((asin(x) + acos(x) + log(x)), tan(x));
+	}
+
+	cout << "z = " << z;
+	return 1;
+}
---
+++ MainProject/MainProject.cpp
@@ -0,0 +1,57 @@
+﻿
+#include <iostream>
+using namespace std;
+int Laboratornaya__1();
+int Laboratornaya__2();
+int Laboratornaya__3();
+int Laboratornaya__4();
+int Laboratornaya__5();
+
+
+
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int n;
+	cout << "Введите номер лабораторной"<<endl;
+	cout << "\n";
+	cout << "ЛАБОРАТОРНАЯ РАБОТА №1 “Программирование линейных и разветвляющихся алгоритмов" << endl;
+	cout << "\n";
+	cout << "ЛАБОРАТОРНАЯ РАБОТА №2 Программирование алгоритмов циклической структуры" << endl;
+	cout << "\n";
+	cout << "ЛАБОРАТОРНАЯ РАБОТА №3  Программирование алгоритмов обработки одномерных массивов" << endl;
+	cout << "\n";
+	cout << "ЛАБОРАТОРНАЯ РАБОТА №4 Обработка двумерных массивов с помощью функций" << endl;
+	cout << "\n";
+	cout << "ЛАБОРАТОРНАЯ РАБОТА №5 Программирование операций над строками и файлами" << endl;
+	cout << "\n";
+	cin >> n;
+	switch (n)
+	{
+	case 1:
+		Laboratornaya__1();
+		break;
+
+	case 2:
+
+		Laboratornaya__2();
+		break;
+	case 3:
+
+		Laboratornaya__3();
+		break;
+
+	case 4:
+
+		Laboratornaya__4();
+		break;
+	case 5:
+
+		Laboratornaya__5();
+		break;
+	}
+
+
+}
+
